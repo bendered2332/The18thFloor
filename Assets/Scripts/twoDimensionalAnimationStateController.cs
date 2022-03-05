3441a1767122efdeb6536be26a0231ff556d1ad1
Edward Bender <eddieb2332@gmail.com>
2022-03-05 13:57:54 -0500

    Revert "Full implementation of 2d blend trees"
    This reverts commit 0cd51089118f13b2eb72a4923287891c559e6e34.

--- Assets/Scripts/twoDimensionalAnimationStateController.cs
+++ Assets/Scripts/twoDimensionalAnimationStateController.cs
@@ -11,22 +11,25 @@ public class TwoDimensionalAnimationStateController : MonoBehaviour
   public float deceleration = 2.0f;
   public float maximumWalkVelocity = 0.5f;
   public float maximumRunVelocity = 2.0f;
-
-  //increase performance
-  int velocityZHash;
-  int VelocityXHash;
   // Start is called before the first frame update
   void Start()
   {
     animator = GetComponent<Animator>();
-
-    velocityZHash = Animator.StringToHash("Velocity Z");
-    VelocityXHash = Animator.StringToHash("Velocity X");
   }
 
-  //handles acceleration and deceleration
-  void changeVelocity(bool forwardPressed, bool leftPressed, bool rightPressed, bool runPressed, float currentMaxVelocity)
+  // Update is called once per frame
+  void Update()
   {
+    //input will be true if the player is pressing on the passed in key parameter
+    //get key input from player
+    bool forwardPressed = Input.GetKey("w");
+    bool leftPressed = Input.GetKey("a");
+    bool rightPressed = Input.GetKey("d");
+    bool runPressed = Input.GetKey("left shift");
+
+    //set current maxVelocity
+    float currentMaxVelocity = runPressed ? maximumRunVelocity : maximumWalkVelocity;
+
     //if player presses forward, increase velocity in z direction
     if (forwardPressed && velocityZ < currentMaxVelocity)
     {
@@ -51,6 +54,12 @@ public class TwoDimensionalAnimationStateController : MonoBehaviour
       velocityZ -= Time.deltaTime * deceleration;
     }
 
+    //reset velocityZ
+    if (!forwardPressed && velocityZ < 0.0f)
+    {
+      velocityZ = 0.0f;
+    }
+
     //increase velocityX if left is not pressed and velocityX < 0
     if (!leftPressed && velocityX < 0.0f)
     {
@@ -61,16 +70,6 @@ public class TwoDimensionalAnimationStateController : MonoBehaviour
     if (!rightPressed && velocityX > 0.0f)
     {
       velocityX -= Time.deltaTime * deceleration;
-    }
-  }
-
-  //handles reset and locking of velocity
-  void lockOrResetVelocity(bool forwardPressed, bool leftPressed, bool rightPressed, bool runPressed, float currentMaxVelocity)
-  {
-    //reset velocityZ
-    if (!forwardPressed && velocityZ < 0.0f)
-    {
-      velocityZ = 0.0f;
     }
 
     //reset velocityX
@@ -156,26 +155,8 @@ public class TwoDimensionalAnimationStateController : MonoBehaviour
     {
       velocityX = currentMaxVelocity;
     }
-  }
 
-  // Update is called once per frame
-  void Update()
-  {
-    //input will be true if the player is pressing on the passed in key parameter
-    //get key input from player
-    bool forwardPressed = Input.GetKey(KeyCode.W);
-    bool leftPressed = Input.GetKey(KeyCode.A);
-    bool rightPressed = Input.GetKey(KeyCode.D);
-    bool runPressed = Input.GetKey(KeyCode.LeftShift);
-
-    //set current maxVelocity
-    float currentMaxVelocity = runPressed ? maximumRunVelocity : maximumWalkVelocity;
-
-    changeVelocity(forwardPressed, leftPressed, rightPressed, runPressed, currentMaxVelocity);
-    lockOrResetVelocity(forwardPressed, leftPressed, rightPressed, runPressed, currentMaxVelocity);
-
-
-    animator.SetFloat(velocityZHash, velocityZ);
-    animator.SetFloat(VelocityXHash, velocityX);
+    animator.SetFloat("Velocity Z", velocityZ);
+    animator.SetFloat("Velocity X", velocityX);
   }
 }
