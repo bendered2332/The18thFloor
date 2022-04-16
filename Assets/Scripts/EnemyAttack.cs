b482be194bce3c9ca5d8c9acc417af051a5748c6
Edward Bender <eddieb2332@gmail.com>
2022-04-15 20:38:46 -0400

    UI and attack changes

--- Assets/Scripts/EnemyAttack.cs
+++ Assets/Scripts/EnemyAttack.cs
@@ -16,13 +16,14 @@ public class EnemyAttack : MonoBehaviour
   [SerializeField] Transform Player;
   [SerializeField] Animator Anim;
   [SerializeField] GameObject Enemy;
-  [SerializeField] float MaxRange = 35.0f;
+  [SerializeField] float MaxRange = 3.0f;
   [SerializeField] int MaxChecks = 3;
   [SerializeField] float ChaseSpeed = 8.5f;
   [SerializeField] float WalkSpeed = 1.6f;
-  [SerializeField] float AttackDIstance = 2.3f;
+  [SerializeField] float AttackDistance = 2.3f;
   [SerializeField] float AttackRotateSpeed = 2.0f;
   [SerializeField] float CheckTime = 3.0f;
+  [SerializeField] GameObject ChaseMusic;
 
 
 
@@ -30,6 +31,7 @@ public class EnemyAttack : MonoBehaviour
   void Start()
   {
     Nav = GetComponentInParent<NavMeshAgent>();
+    ChaseMusic.gameObject.SetActive(false);
   }
 
   // Update is called once per frame
@@ -48,21 +50,54 @@ public class EnemyAttack : MonoBehaviour
         {
           Debug.Log("I can see the player.");
           RunToPlayer = true;
+          FailedChecks = 0;
         }
         if (blocked == true)
         {
           Debug.Log("Where did the player go?");
           RunToPlayer = false;
           Anim.SetInteger("State", 1);
+          FailedChecks++;
         }
 
         StartCoroutine(TimeCheck());
       }
-
     }
+
     if (RunToPlayer == true)
     {
+      Enemy.GetComponent<EnemyMove>().enabled = false;
+      ChaseMusic.gameObject.SetActive(true);
+      if (DistanceToPlayer > AttackDistance)
+      {
+        Nav.isStopped = false;
+        Anim.SetInteger("State", 2);
+        Nav.acceleration = 24;
+        Nav.SetDestination(Player.position);
+        Nav.speed = ChaseSpeed;
+      }
+      if (DistanceToPlayer < AttackDistance - 0.5f)
+      {
+        Nav.isStopped = true;
+        Debug.Log("I am attacking");
+        Anim.SetInteger("State", 3);
+        Nav.acceleration = 180;
+        Vector3 Pos = (Player.position - Enemy.transform.position).normalized;
+        Quaternion PosRotation = Quaternion.LookRotation(new Vector3(Pos.x, 0, Pos.z));
+        Enemy.transform.rotation = Quaternion.Slerp(Enemy.transform.rotation, PosRotation, Time.deltaTime * AttackRotateSpeed);
+      }
+    }
+    else if (RunToPlayer == false)
+    {
+      Nav.isStopped = true;
+    }
+  }
 
+  private void OnTriggerEnter(Collider other)
+  {
+    if (other.gameObject.CompareTag("Player"))
+    {
+      RunToPlayer = true;
     }
   }
 
@@ -70,5 +105,14 @@ public class EnemyAttack : MonoBehaviour
   {
     yield return new WaitForSeconds(CheckTime);
     IsChecking = true;
+
+    if (FailedChecks > MaxChecks)
+    {
+      Enemy.GetComponent<EnemyMove>().enabled = true;
+      Nav.isStopped = false;
+      Nav.speed = WalkSpeed;
+      FailedChecks = 0;
+      ChaseMusic.gameObject.SetActive(false);
+    }
   }
 }
